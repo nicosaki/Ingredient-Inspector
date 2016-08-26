8369de7edaa3fe58bf22b877fde0e8dfa498e1e4
Nicole Iwasaki <Nicole.A.Iwasaki@gmail.com>
2016-08-25 18:02:02 -0700

    Ingredient View container stuff

--- src/modules/scanner/IngredientViewContainer.js
+++ src/modules/scanner/IngredientViewContainer.js
@@ -1,11 +1,12 @@
 import {connect} from 'react-redux';
-import Ingredient from './IngredientView';
+import IngredientView from './IngredientView';
 
 // pass the counter's value to the component as a prop called `value`.
 // Because we omit the second parameter, the `dispatch` function is
 // automatically passed as a prop.
 export default connect(
   state => ({
-    barcode: state.getIn(['scanner', 'barcode'])
+    barcode: state.getIn(['scanner', 'barcode']),
+    data: state.getIn['counter', 'data']
   })
-)(Ingredient);
+)(IngredientView);
